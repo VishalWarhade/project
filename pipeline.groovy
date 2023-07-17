8d13e99f89eaae60cb98185a49690a2ce42b615c
VishalWarhade <115239850+VishalWarhade@users.noreply.github.com>
2023-07-17 19:32:01 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,22 +1,22 @@
-pipeline {
-    agent any
-    stages {
-        stage ('pull') {
-            steps{
-                git 'https://github.com/VishalWarhade/project.git'
-            }
-        }
+// pipeline {
+//     agent any
+//     stages {
+//         stage ('pull') {
+//             steps{
+//                 git 'https://github.com/VishalWarhade/project.git'
+//             }
+//         }
         
-        stage ('deploy') {
-            steps {
-                sh 'mv /home/centos/workspace/job1/index.html /usr/share/nginx/html/index.html'
-            }
-        }
-        stage ('restart nginx') {
-            steps{
-                sh 'sudo systemctl restart nginx'
-            }
-        }
+//         stage ('deploy') {
+//             steps {
+//                 sh 'mv /home/centos/workspace/job1/index.html /usr/share/nginx/html/index.html'
+//             }
+//         }
+//         stage ('restart nginx') {
+//             steps{
+//                 sh 'sudo systemctl restart nginx'
+//             }
+//         }
 
-    }
-}
+//     }
+// }
